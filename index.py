8026a8cbcda134ce61ff5221879ec9f6e77220a7
Gibril Idrisu Issaka <gibrilissakaiddrisu@gmail.com>
2023-12-19 12:37:04 +0000

    get messages

---
+++ index.py
@@ -0,0 +1,53 @@
+import os
+import asyncio
+from telethon import TelegramClient
+from dotenv import load_dotenv
+load_dotenv()
+
+
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+
+client = TelegramClient('me', api_id, api_hash)
+
+async def get_channels():
+    # First of all, get all the channels and their ID's and store 
+    channels = []
+    async for dialog in client.iter_dialogs():
+        channels+=[{
+            'channel_id': dialog.id,
+            'channel_name': dialog.name
+        }]
+    
+    return channels
+
+def get_channel_id(channels, name):
+    # print(channels)
+    for channel in channels:
+        print(channel)
+        if name == channel['channel_name']:
+            return channel['channel_id']
+    # else:
+    #     return None
+
+async def channel_messages(id):
+    async for message in client.iter_messages(id):
+        print(message.id, message.text)
+    
+
+
+
+async def main():
+    async with client:
+        channel_data = await get_channels()
+
+        desired_channel = input('Which channel messages do you want to scrape:\n ')
+        channel_id = get_channel_id(channel_data, desired_channel)
+        
+        if channel_id:
+            await channel_messages(channel_id)
+        else:
+            print('Channel not found.')
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ main.py
@@ -0,0 +1,72 @@
+import os
+from telethon import TelegramClient
+from dotenv import load_dotenv
+load_dotenv()
+# Remember to use your own values from my.telegram.org!
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+
+client = TelegramClient('me', api_id, api_hash)
+
+async def main():
+    # Getting information about yourself
+    me = await client.get_me()
+
+    # "me" is a user object. You can pretty-print
+    # any Telegram object with the "stringify" method:
+    print(me.stringify())
+
+    # When you print something, you see a representation of it.
+    # You can access all attributes of Telegram objects with
+    # the dot operator. For example, to get the username:
+    username = me.username
+    print(username)
+    print(me.phone)
+
+    # You can print all the dialogs/conversations that you are part of:
+    async for dialog in client.iter_dialogs():
+        await client.send_message(dialog.id, 'This message is from Gibril')
+        print('Message has been sent')
+        # print(dialog.name, 'has ID', dialog.id)
+
+    # You can send messages to yourself...
+    await client.send_message('me', 'Hello, myself, Hehehe!')
+    # ...to some chat ID
+    # await client.send_message(-100123456, 'Hello, group!')
+    # # ...to your contacts
+    # await client.send_message('+34600123123', 'Hello, friend!')
+    # # ...or even to any username
+    # await client.send_message('username', 'Testing Telethon!')
+
+    # You can, of course, use markdown in your messages:
+    message = await client.send_message(
+        'me',
+        'This message has **bold**, `code`, __italics__ and '
+        'a [nice website](https://example.com)!',
+        link_preview=False
+    )
+
+    # Sending a message returns the sent message object, which you can use
+    # print(message.raw_text)
+
+    # # You can reply to messages directly if you have a message object
+    # await message.reply('Cool!')
+
+    # # Or send files, songs, documents, albums...
+    # await client.send_file('me', '/home/me/Pictures/holidays.jpg')
+
+    # # You can print the message history of any chat:
+    # async for message in client.iter_messages('me'):
+    #     print(message.id, message.text)
+
+    #     # You can download media from messages, too!
+    #     # The method will return the path where the file was saved.
+    #     if message.photo:
+    #         path = await message.download_media()
+    #         print('File saved to', path)  # printed after download is done
+
+# with client:
+#     client.loop.run_until_complete(main())
+
+with client:
+    client.loop.run_until_complete(main())
