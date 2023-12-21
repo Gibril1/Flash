3cbb917ec8023b325fa87ac4812d17ac001bd122
Gibril Idrisu Issaka <gibrilissakaiddrisu@gmail.com>
2023-12-21 09:38:23 +0000

    channel groups refactor

--- index.py
+++ index.py
@@ -12,23 +12,16 @@ client = TelegramClient('me', api_id, api_hash)
 
 
 
-async def get_channels():
+async def get_channels_and_groups():
     # First of all, get all the channels and their ID's and store 
     channels = []
-    groups = []
     async for dialog in client.iter_dialogs():
-        if dialog.is_channel:
+        if dialog.is_channel or dialog.is_group:
             channels+=[{
                 'channel_id': dialog.id,
                 'channel_name': dialog.name
             }]
-
-        elif dialog.is_group:
-            groups+=[{
-                'group_id': dialog.id,
-                'group_name': dialog.name
-            }]
-    print(channels, groups)
+    
     return channels
 
 def get_channel_id(channels, name):
@@ -38,26 +31,56 @@ def get_channel_id(channels, name):
     return None
 
 async def channel_messages(id):
+    messages = []
     async for message in client.iter_messages(id):
-        # print(message)
-        print(message.id, message.text)
+        messages+=[{
+            'message_id': message.id,
+            'message': message.text
+        }]
+    return messages
     
 
-
+async def get_channel_subscribers(id):
+    member_details = []
+    participants = await client.get_participants(id, aggressive=True)
+    for participant in participants:
+        member_details += [{
+            'first_name': participant.first_name ,
+            'last_name': participant.last_name,
+            'phone_number': participant.phone,
+            'username': participant.username
+        }]
+    return member_details
 
 async def main():
     async with client:
-        channel_data = await get_channels()
-        # print(channel_data)
+        channel_data = await get_channels_and_groups()
+
+        # Print the channels 
+        print('\n\nSee the list of channels you can choose from')
         for index, channel in enumerate(channel_data):
             print(f"{index}. {channel['channel_name']}\n")
-        desired_channel = input('Which channel messages do you want to scrape:\n ')
-        channel_id = get_channel_id(channel_data, desired_channel)
+    
+        # Prompt the user for the channel name
+        desired_channel = input('Which channel or group messages do you want to scrape:\n ')
+
+        channel_id = get_channel_id(channel_data, desired_channel)  
+        while not channel_id:
+            # Prompt the user for the channel name
+            print('\n You did not enter the right channel name. Try Again...\n')
+            desired_channel = input('Which channel messages do you want to scrape:\n ')
+            channel_id = get_channel_id(channel_data, desired_channel)
         
-        if channel_id:
-            await channel_messages(channel_id)
-        else:
-            print('Channel not found.')
+        
+        messages = await channel_messages(channel_id)
+        # Display the messages in the channel to the user
+        print('These are the members of the channels')
+        participants = await get_channel_subscribers(channel_id)
+        print(participants)
+        
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
+
